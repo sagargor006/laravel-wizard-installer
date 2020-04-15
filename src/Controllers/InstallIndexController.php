76296fa665c7905824c79b2899374701c4c0a16b
David Cortés <dacoto97@hotmail.com>
2020-04-16 01:56:49 +0200

    Removed cache after install

--- src/Controllers/InstallIndexController.php
+++ src/Controllers/InstallIndexController.php
@@ -5,6 +5,7 @@ namespace dacoto\LaravelInstaller\Controllers;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\View\View;
 use Jackiedo\DotenvEditor\Facades\DotenvEditor;
 
@@ -29,6 +30,11 @@ class InstallIndexController extends Controller
     {
         DotenvEditor::setKey('INSTALLED', true);
         DotenvEditor::save();
+        Artisan::call('route:cache');
+        Artisan::call('cache:clear');
+        Artisan::call('config:cache');
+        Artisan::call('optimize:clear');
+        Artisan::call('auth:clear-resets');
         return view('installer::steps.finish');
     }
 }
