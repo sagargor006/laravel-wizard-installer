94db8b3e08c9524d5c809ea95834548948678ed1
David Cortés <cortestoledanodavid@gmail.com>
2020-09-09 18:43:18 +0200

    Adjusted env editor

--- src/Support/EnvEditor.php
+++ src/Support/EnvEditor.php
@@ -2,22 +2,13 @@
 
 namespace dacoto\LaravelInstaller\Support;
 
+use SetEnv;
+
 class EnvEditor
 {
     public static function setEnv($key, $value = null): void
     {
-        $path = base_path('.env');
-        if (is_bool(env($key))) {
-            $old = env($key) ? 'true' : 'false';
-        } elseif (env($key) === null) {
-            $old = 'null';
-        } else {
-            $old = env($key);
-        }
-        if (file_exists($path)) {
-            file_put_contents($path, str_replace(
-                "$key=".$old, "$key=".$value, file_get_contents($path)
-            ));
-        }
+        SetEnv::setKey($key, $value);
+        SetEnv::save();
     }
 }
